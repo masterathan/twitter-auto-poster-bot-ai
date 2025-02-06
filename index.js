4c3dda19aad9f03d04ab39397f0507bb865e716e
Masterathan <140475003+masterathan@users.noreply.github.com>
2025-02-06 18:42:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ async function run() {
 
   // Write your prompt here
   const prompt =
-    "generate a content insights, thoughts, trvia for $BERA, BERACHAIN, Chainbase, Story Protocol additionally add emoji on the thread";
+    "generate a content insights, thoughts, trvia for $BERA, BERACHAIN, Chainbase, Story Protocol additionally add emoji on the thread keep it short and specific and unique";
 
   const result = await model.generateContent(prompt);
   const response = await result.response;
