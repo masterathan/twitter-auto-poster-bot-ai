bf6a33ec8c94e180f52eb80a2d760f44b5ca2f90
Masterathan <140475003+masterathan@users.noreply.github.com>
2025-05-06 17:11:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ async function run() {
 
   // Write your prompt here
   const prompt =
-    "generate a web development content, tips and tricks or something new or some rant or some advice as a tweet, it should not be vague and should be unique; under 280 characters and should be plain text, you can use emojis";
+    "Generate a content for @ikadotxyz and @GiveRep. Keep the content in one sentence and additionally add unique emoji on the thread keep it short and specific. Limit the word to 250 characters. Most importantly create a none repetitive content and avoid being used again, be normal, be humanize, and avoid using () in the sentence [ keep it all in one sentence ]";
 
   const result = await model.generateContent(prompt);
   const response = await result.response;
